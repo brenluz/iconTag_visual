4dade61474abcb0b772be14a217dc08e5243ad51
edu-wmv <montzdudu@outlook.com>
2023-11-05 00:56:45 -0300

    fix: img-agent-load

--- agent.js
+++ agent.js
@@ -1,58 +1,36 @@
 class Agent extends Vehicle {
-  constructor(
-    name,
-    initialEnergy,
-    foodEnergy,
-    lostEnergy,
-    maxEnergy,
-    reprodProb,
-    reprodNumber,
-    visionR,
-    maxSpeed,
-    r,
-    img
-  ) {
-    // constructor code here
+  constructor(name, maxSpeed, imgSource) {
 
     // initial position
-    let x = Math.random() * (width - 1) + 1;
-    let y = Math.random() * (height - 1) + 1;
+    let x = Math.random() * (windowWidth - 1) + 1;
+    let y = Math.random() * (windowHeight - 1) + 1;
     super(x, y);
+    
     // parameters
     this.name = name;
-    this.energy = initialEnergy;
-    this.foodEnergy = foodEnergy;
-    this.lostEnergy = lostEnergy;
-    this.maxEnergy = maxEnergy;
-    this.reprodProb = reprodProb;
-    this.reprodNumber = reprodNumber;
-    this.visionR = visionR;
     this.maxSpeed = maxSpeed;
-    this.r = r;
-    if (img != null) {
-      this.img = loadImage(img);
-    }
+    this.imgSource = imgSource != '' ? loadImage(imgSource) : loadImage('https://placehold.co/50X50?text=ICON');
+  
+    this.mask = createGraphics(50, 50);
+    this.mask.ellipse(0, 0, 50, 50); // Draw a circle in the mask
+    this.imgSource.mask(this.mask); // Apply the mask to the image
   }
 
   show() {
-    push() // starting a new drawing state
-     translate(this.pos.x, this.pos.y) // displacing the cursor by pos.x and pos.y
-     fill(56, 56, 56) // setting fill color as rgb(56, 56, 56)
-     strokeWeight(1) // setting stroke weight as 1
-     // displaying the triangle with:
-     // vertex 1: (-radius, -radius / 2)
-     // vertex 2: (-radius, radius / 2)
-     // vertex 3(radius, 0)
-     // remember that this is displaced by pos.x and pos.y because of `translate()`
-     
-     text(this.name, 0, -this.r - 8); // displaying the breno's name above the triangle
-     noFill()
-     noStroke()
-     rect(0,0,50,50)
-     if (this.img != null) {
-       image(this.img,0,0,50,50)
-     }
+    push()
+    
+    translate(this.pos.x, this.pos.y) 
+    
+    fill(56, 56, 56)
+    strokeWeight(1) 
+    rect(0,0,50,50)
 
-     pop() // restoring the previous drawing state
+    imageMode(CORNERS)
+    image(this.imgSource, 0, 0, 50, 50) 
+
+    text(this.name, 0, 65); 
+    noFill()
+
+    pop()
 }
 }
